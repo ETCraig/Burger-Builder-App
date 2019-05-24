948c3c2135f4ebf523a1321366c840ab2fcd30e9
unknown <devethancraig@gmail.com>
2019-05-24 17:25:47 -0600

    Structure Started - Build Process Started

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Layout.css';
+
+import Aux from '../../hoc/Aux'
+
+const Layout = (props) => (
+    <Aux>
+        <div>
+            Toolbar - SideDrawer - Backdrop
+        </div>
+        <main className="Content">
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default Layout;
