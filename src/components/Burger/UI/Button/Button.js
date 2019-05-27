00d2e538f5dedd20f40765dcc03f3ad8c02b5402
unknown <devethancraig@gmail.com>
2019-05-26 20:37:03 -0600

    Modal Buttons and Backdrop Finished

---
+++ src/components/Burger/UI/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+
+const button = (props) => (
+    <button
+        className='Button'
+        style={{color: props.btnType === 'Danger' ? '#944317' : '#5C9210'}}
+        onClick={props.clicked}    
+    >{props.children}</button>
+);
+
+export default button;
