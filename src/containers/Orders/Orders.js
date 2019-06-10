b854d02b2db0bd811244392c6c61fc5c96bea45c
unknown <devethancraig@gmail.com>
2019-06-09 20:52:23 -0600

    Orders Call and Props Manipulation Finished

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,11 +1,35 @@
 import React, {Component} from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/ErrorHandler';
+import order from '../../components/Order/Order';
 
 class Order extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+    componentDidMount() {
+        axios.get('/orders').then(res => {
+            const fetchedOrders = [];
+            for(let key in res.data) {
+                fetchedOrders.push({
+                    ...res.data[key],
+                    id: key
+                });
+            }
+            this.setState({loading: false, orders: fetchedOrders});
+        }).catch(err => {
+            this.setState({loading: false});
+        });
+    }
     render() {
         return(
             <div>
+                {this.state.orders.map(order => {
+                    <Order key={order.id} ingredients={order.ingredients} price={+order.price} />
+                })}
                 <Order />
                 <Order />
             </div>
@@ -13,4 +37,4 @@ class Order extends Component {
     }
 }
 
-export default Order;
+export default withErrorHandler(Order, axios);
